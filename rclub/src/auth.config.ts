8a412a316b55be3ce5c382e9a70d8f38082fa045
Sun-droid <Sun-droid@tmp.se>
2024-08-20 21:54:17 +0200

    Preparing for deployment

--- rclub/src/auth.config.ts
+++ rclub/src/auth.config.ts
@@ -2,7 +2,7 @@ import type {NextAuthConfig} from 'next-auth';
 
 export const authConfig = {
     pages: {
-        signIn: '/admin/login',
+        signIn: '/?signmodal=true',
     },
     callbacks: {
         authorized({auth, request: {nextUrl}}) {
