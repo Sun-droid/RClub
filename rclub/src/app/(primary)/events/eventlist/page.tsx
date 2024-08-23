ab7abbb7e1d39657ae33c7efc2077a1545520f7b
Sun-droid <Sun-droid@tmp.se>
2024-08-23 13:26:02 +0200

    Preparing for deployment

--- rclub/src/app/(primary)/events/eventlist/page.tsx
+++ rclub/src/app/(primary)/events/eventlist/page.tsx
@@ -1,3 +1,4 @@
+//import {promises as fs} from "fs";
 //import fs from "fs";
 import {promises as fs} from "fs";
 import Card from "@/app/components/card";
@@ -9,21 +10,30 @@ import {auth} from '@/auth'
 let c = 0;
 export default async function Page() {
     const session = await auth()
+    console.log("session  eventlist", session)
 
 //    const fileDefault = await fs.readFile(
 //        process.cwd() + "/src/app/(primary)/events/srcFiles/DefaultCard.json",
 //        "utf8"
 //    );
 
+//    const fileByAdmin = await fs.readFile(
+//        process.cwd() + "/src/app/(primary)/database/EventsData1.json",
+//        "utf8"
+//    );
+
+
     const fileDefault = await fs.readFile(path.join(
         process.cwd(), "src", "app", "(primary)", "events", "srcFiles", "DefaultCard.json",
     ), "utf8");
 
 
-    const fileByAdmin = await fs.readFile(
-        process.cwd() + "/src/app/(primary)/database/EventsData1.json",
-        "utf8"
+    const fileByAdmin = await fs.readFile(path.join(
+            process.cwd(), "src", "app", "(primary)", "database", "EventsData1.json",
+        ), "utf8"
     );
+
+
 
 
     const dataDefault = JSON.parse(fileDefault);
