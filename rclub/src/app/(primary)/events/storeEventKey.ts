e5286e188da2e4a96086cc61fa0e640abf1276f6
Sun-droid <Sun-droid@tmp.se>
2024-08-28 22:31:10 +0200

    Preparing for deployment

--- rclub/src/app/(primary)/events/storeEventKey.ts
+++ rclub/src/app/(primary)/events/storeEventKey.ts
@@ -1,4 +1,4 @@
-import {promises as fs, readFileSync} from 'fs';
+import {promises as fs} from 'fs';
 import path from 'path';
 import {kv} from '@vercel/kv';
 //import { savedAddedValKey } from './savedAddedValKey';
@@ -9,10 +9,17 @@ import {savedAddedValKey} from '@/app/(primary)/events/savedAddedValKey';
 
 export async function storeEventKey() {
 //    const fileEventsKey = await fs.readFile(eventsDataKey, 'utf8');
-    const fileEventsKey = await /*fs.readFile(*/ readFileSync( path.join(
-            process.cwd(), "/src/app/(primary)/database/ReserveObjectKey.json",
-        ), "utf8"
-    );
+
+
+    const fileEventsKeyPath = path.join(process.cwd(), '/src/app/(primary)/database/ReserveObjectKey.json')
+//    const fileEventsKey = await fs.readdir(fileEventsKeyPath,'utf8')
+    const fileEventsKey = await fs.readFile(fileEventsKeyPath,'utf8')
+
+
+//    const fileEventsKey = await fs.readFile(path.join(
+//            process.cwd(), "/src/app/(primary)/database/ReserveObjectKey.json",
+//        ), "utf8"
+//    );
 
 //const fileUserJsonPath = path.join(process.cwd(), '/src/app/lib/data/users.json');
 //const fileUserJson = await fs.readFile(fileUserJsonPath, 'utf8')
